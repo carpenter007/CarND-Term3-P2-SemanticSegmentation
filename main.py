0a0deebca073077a0bc7d878d44f62cc99d0b3ab
Unknown <elias@zimi.de>
2018-08-08 23:15:06 +0200

    reload best aproach with 50 epochs, batch size of 5 and learning rate of 0.001

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
 
     for epoch in range(epochs):
         for image, label in get_batches_fn(batch_size):
-             _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image: image, correct_label: label, keep_prob: 0.5, learning_rate: 0.0008})
+             _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image: image, correct_label: label, keep_prob: 0.5, learning_rate: 0.001})
 
         print("Epoch " + str(epoch) + ", Minibatch Loss= " + \
               "{:.4f}".format(loss))
@@ -158,8 +158,8 @@ def run():
 
         # Build NN using load_vgg, layers, and optimize function
 
-        epochs = 105
-        batch_size = 4
+        epochs = 50
+        batch_size = 5
 
         # TF place holders:
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
